32c317140fcd92d171accc991af3cef79681dd9d
Shawn Zheng <shawnz@checkpay.ca>
2021-12-09 20:52:48 -0800

    added line chart

--- screens/CoinDetailScreen.tsx
+++ screens/CoinDetailScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Image, TouchableOpacity, FlatList, Dimensions } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, FlatList, Dimensions, Pressable, Button } from 'react-native';
 
 import { increamentPercentage, moneyFormatter } from '../components/helpers/moneyHelpers';
 import { Text, View } from '../components/Themed';
@@ -7,9 +7,35 @@ import { RootTabScreenProps } from '../types';
 
 import { Feather } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
+
+const chartData = [
+  { x: -2, y: 15 },
+  { x: -1, y: 10 },
+  { x: 0, y: 12 },
+  { x: 1, y: 7 },
+  { x: 2, y: 6 },
+  { x: 3, y: 8 },
+  { x: 4, y: 10 },
+  { x: 5, y: 8 },
+  { x: 6, y: 12 },
+  { x: 7, y: 14 },
+  { x: 8, y: 12 },
+  { x: 9, y: 13.5 },
+  { x: 10, y: 18 },
+]
+
 
 export default function CoinDetailScreen({ navigation }: RootTabScreenProps<'CoinDetail'>) {
   const isUp = true;
+  const timePriodSlots = ['1H', '1D', '1W', '1M', '1Y']
+  const [pressedTimePeriodIdx, setPressedTimePeriodIdx] = React.useState<number>(0);
+  const [priceTrende, setPriceTrende] = React.useState<{x: number, y: number}[]>(chartData)
+
+  React.useEffect(() => {
+    const rdm = Array.from({length: 20}, (_, idex) => ({x: idex, y: Math.floor(Math.random() * 20)}))
+    setPriceTrende(rdm)
+  },[])
 
   return (
     <View style={styles.container}>
@@ -19,9 +45,33 @@ export default function CoinDetailScreen({ navigation }: RootTabScreenProps<'Coi
         <Feather name={isUp ? "chevron-up" : 'chevron-down'} size={18} color={isUp ? '#70cf77' : '#cf7070'} />
         <Text style={{color: isUp ? '#70cf77' : '#cf7070', fontSize: 18}}>{increamentPercentage(0.0797)}</Text>
       </View>
-      <View style={{width: "100%", backgroundColor: '#fafa', height: Dimensions.get('window').height / 2}} />
-      <View>
-        <Text>1H ...</Text>
+
+      <Chart
+        style={{ height: Dimensions.get('window').height / 2, width: "100%" }}
+        data={priceTrende}
+        xDomain={{ min: 0, max: 20 }}
+        yDomain={{ min: 0, max: 20 }}
+      >
+
+        <Line theme={{ stroke: { color: '#129FFF', width: 3 },  }} />
+      </Chart>
+
+      <View style={{flexDirection: 'row'}}>
+        {
+          timePriodSlots.map((time, idx) => {
+            return (
+              <View 
+                style={styles.timePeriodSlotsCtnr}
+              >
+                <TouchableOpacity 
+                  onPress={() => setPressedTimePeriodIdx(idx)}
+                >
+                  <Text style={{color: idx === pressedTimePeriodIdx? '#129FFF': '#fff'}}>{time}</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          })
+        }
       </View>
       <View style={styles.divider} />
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
@@ -29,6 +79,16 @@ export default function CoinDetailScreen({ navigation }: RootTabScreenProps<'Coi
         <Text>{moneyFormatter(12.45)}</Text>
       </View>
       <View style={styles.divider} />
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.title}>Nothing here...</Text>
+        <Text style={styles.subTitle}>Start by adding some funds.</Text>
+        <TouchableOpacity
+          style={styles.bigBlueBtn}
+        >
+          <Text style={{color: "#fff", fontSize: 20, fontWeight: 'bold'}}>Add funds</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -43,10 +103,28 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  subTitle: {
+    fontSize: 15,
+    color: '#ccc',
+    marginVertical: 10,
+  },
   divider: {
     marginVertical: 10,
     height: 1,
     width: '100%',
     backgroundColor: '#ccc'
+  },
+  timePeriodSlotsCtnr: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  bigBlueBtn: {
+    width: 355, 
+    height: 50, 
+    borderRadius: 10,
+    backgroundColor: '#129fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
